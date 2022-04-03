74494296b0e5b78fe736ccf69b58e1b3e17c3fe1
brodyking <bk.2k@hotmail.com>
2022-04-03 02:13:54 -0500

    Finished and Polished tabs

--- app/site.js
+++ app/site.js
@@ -1,9 +1,35 @@
+// Open Tab Function
 function openTab(tabName) {
     var i;
     var x = document.getElementsByClassName("tabcontent");
     for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
+        x[i].style.display = "none";
     }
     document.getElementById(tabName).style.display = "block";
-  }
-  
+}
+
+// Nav Tab Links
+    function changeTabLinkActive(tabName) {
+        document.getElementById("devTabLink").classList.remove('active');
+        document.getElementById("testTabLink").classList.remove('active');
+        document.getElementById("optionsTabLink").classList.remove('active');
+        document.getElementById(tabName).setAttribute('class','nav-link active');
+    }
+    // Dev Tab
+        document.getElementById("devTabLink").onclick = function() {
+            openTab('devTab')
+            changeTabLinkActive("devTabLink");
+        };
+    // Test Tab
+        document.getElementById("testTabLink").onclick = function() {
+            openTab('testTab')
+            changeTabLinkActive("testTabLink");
+        };
+    // Options Tab
+        document.getElementById("optionsTabLink").onclick = function() {
+            openTab('optionsTab')
+            changeTabLinkActive("optionsTabLink");
+        };
+    // Start First Page
+    openTab('devTab');
+    changeTabLinkActive("devTabLink");
